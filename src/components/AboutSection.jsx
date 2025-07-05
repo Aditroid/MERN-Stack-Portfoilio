9b4ccb53aba715cd7668c0405aa8115daf7d0d0a
Aditroid <bora.aditya786@gmail.com>
2025-07-05 20:42:34 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1NNnDFfnaU7XMcKwB-sHVtm_rAFXqMf6c/view?usp=sharing"
+                href="https://drive.google.com/file/d/1NNnDFfnaU7XMcKwB-sHVtm_rAFXqMf6c/view?usp=sharing" target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
